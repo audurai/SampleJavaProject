4f3df53f3038c7c1d6eb9e28e531dfd7e816518b
audurai <audurai@gmail.com>
2015-02-27 08:48:23 +0530

    Testing commit for the local repo

---
+++ src/com/audu/sample/MainClass.java
@@ -0,0 +1,10 @@
+package com.audu.sample;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tesing Main Class");
+	}
+
+}
